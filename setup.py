6087087a5da91d2d0813b06084eac1feb5b5bd55
umidev <techlog@urbanmapping.com>
2009-04-03 06:28:41 +0000

    Upped the version; this code been vetted for a good long while.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name='ligeos',
-      version='0.0.1',
+      version='0.1.0',
       #install_requires=['cython>=0.10.3'], 
       packages=['ligeos',],
       ext_modules = [Extension("ligeos.linearref", ["ligeos/linearref.pyx"])],
